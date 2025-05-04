b0a6c1c8dbd75499158807419bc2309452ae1fd9
JoaoSacala <joaosacala88@gmail.com>
2025-05-04 21:33:41 +0100

    update metadata

--- nextjs/src/app/routes/getCustomer.ts
+++ nextjs/src/app/routes/getCustomer.ts
@@ -5,19 +5,21 @@ interface CustomerProps {
     id: string
   }
   
-  /* export async function getCustomer(): Promise<CustomerProps[]> {
+export async function getCustomer(): Promise<CustomerProps[]> {
 
-    const response = await api.get<CustomerProps[]>('/customers')
-    revalidateTag('customer')
-    return response.data;
+    const response = await fetch(`${process.env.BASE_URL}/customers`, {
+      next: { tags: ['customers'] },
+    })
+    const data = await response.json();
+    return data as CustomerProps[];
 
-  } */
+  } 
 
     // getCustomer.ts
-export async function getCustomer(): Promise<CustomerProps[]> {
+/* export async function getCustomer(): Promise<CustomerProps[]> {
   const res = await fetch(`${process.env.BASE_URL}/customers`, {
     next: { tags: ['customers'] }, // ✅ essa tag será revalidada quando necessário
   })
   const data = await res.json();
   return data as CustomerProps[];
-}
+} */
